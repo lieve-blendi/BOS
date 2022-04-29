880fe05ce38ab5da64ff8f1f7da81a6ceca3fece
IonutParau <46377726+IonutParau@users.noreply.github.com>
2022-04-29 14:16:51 +0200

    Diagnostics

--- firmware.lua
+++ firmware.lua
@@ -27,61 +27,66 @@ do
     if not gpu then
         error("No graphics card available")
     end
-
-    while true do
-        local function tryLoadFrom(addr)
-            local width, height = boot_invoke(gpu, "getResolution")
-            boot_invoke(gpu, "fill", 1, 1, width, height)
-            boot_invoke(gpu, "set", 1, 1, "Booting " .. addr .. "...")
-            computer.setBootAddress(addr)
-            local handle, reason = boot_invoke(address, "open", "/init.lua")
-            if not handle then
-                return nil, reason
-            end
-            local buffer = ""
-            repeat
-                local data, reason = boot_invoke(address, "read", handle, math.huge)
-                if not data and reason then
-                    return nil, reason
-                end
-                buffer = buffer .. (data or "")
-            until not data
-            boot_invoke(address, "close", handle)
-            local loadedOS, reason = load(buffer, "=init")
-            if not loadedOS then
-                error("Failed to load OS: " .. reason)
-            end
-            loadedOS()
-        end
-
+    local function tryLoadFrom(addr)
         local width, height = boot_invoke(gpu, "getResolution")
         boot_invoke(gpu, "fill", 1, 1, width, height)
-        boot_invoke(gpu, "set", 1, 1, "Choose boot drive: ")
-
-        local fs = {}
-
-        for fileSys in component.list("filesystem") do
-            table.insert(fs, fileSys)
+        boot_invoke(gpu, "set", 1, 1, "Booting " .. addr .. "...")
+        computer.setBootAddress(addr)
+        local handle, reason = boot_invoke(address, "open", "/init.lua")
+        if not handle then
+            return nil, reason
         end
-
-        for i=1,#fs do
-            boot_invoke(gpu, "set", 1, i+1, ">" .. fs[i])
-        end
-
-        local e, addr, char, code = computer.pullSignal()
-
-        if e == "key_down" then
-            if code == 28 then
-                tryLoadFrom(computer.getBootAddress())
-            elseif code == 2 then
-                tryLoadFrom(fs[1])
-            elseif code == 3 then
-                tryLoadFrom(fs[2])
-            elseif code == 4 then
-                tryLoadFrom(fs[3])
-            elseif code == 5 then
-                tryLoadFrom(fs[4])
+        local buffer = ""
+        repeat
+            local data, reason = boot_invoke(address, "read", handle, math.huge)
+            if not data and reason then
+                return nil, reason
             end
+            buffer = buffer .. (data or "")
+        until not data
+        boot_invoke(address, "close", handle)
+        local loadedOS, reason = load(buffer, "=init")
+        if not loadedOS then
+            error("Failed to load OS: " .. reason)
         end
+        loadedOS()
+    end
+
+    local width, height = boot_invoke(gpu, "getResolution")
+    boot_invoke(gpu, "fill", 1, 1, width, height)
+    boot_invoke(gpu, "set", 1, 1, "Choose boot drive: ")
+
+    local fs = {}
+
+    for fileSys in component.list("filesystem") do
+        table.insert(fs, fileSys)
+    end
+
+    for i=1,#fs do
+        boot_invoke(gpu, "set", 1, i+1, tostring(i) .. ". " .. fs[i])
+    end
+
+    boot_invoke(gpu, "set", 1, #fs+2, "Press keys 1-4 to boot")
+    boot_invoke(gpu, "set", 1, #fs+3, "Press enter to boot to default drive")
+    boot_invoke(gpu, "set", 1, #fs+3, "Press any other key to reboot")
+
+    local e, addr, char, code
+    
+    repeat
+        e, addr, char, code = computer.pullSignal()
+    until e == "key_down"
+
+    if code == 28 then
+        tryLoadFrom(computer.getBootAddress())
+    elseif code == 2 then
+        tryLoadFrom(fs[1])
+    elseif code == 3 then
+        tryLoadFrom(fs[2])
+    elseif code == 4 then
+        tryLoadFrom(fs[3])
+    elseif code == 5 then
+        tryLoadFrom(fs[4])
+    else
+        computer.shutdown(true)
     end
 end
