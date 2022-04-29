c9c1bf8455c35ad566ad6424786dd34f37b64503
lieve-blendi <blendiappel@gmail.com>
2022-04-29 21:27:11 +0200

    lots of stuff (and i hope installer works)

--- Installer.lua
+++ Installer.lua
@@ -4,9 +4,26 @@ local repoPath = "https://raw.githubusercontent.com/lieve-blendi/BOS/main"
 
 local filePaths = {
   "/init.lua",
-  "/libs",
-  "/libs/prints.lua",
+  "/drivers",
+  "/drivers/keyboard.lua",
+  "/drivers/rgpu.lua",
+  "/drivers/text.lua",
+  "/desktops/Bird.lua",
+  "/shells/Carrot.lua"
 }
+
+local function getComponentAddress(name)
+	return component.list(name)() or error("Required " .. name .. " component is missing")
+end
+
+local function getComponentProxy(name)
+	return component.proxy(getComponentAddress(name))
+end
+
+local EEPROMProxy, internetProxy, GPUProxy = 
+	getComponentProxy("eeprom"),
+	getComponentProxy("internet"),
+	getComponentProxy("gpu")
 
 local function isLuaScript(path)
   return (path:sub(-4) == ".lua")
@@ -27,12 +44,24 @@ for _, p in ipairs(filePaths) do
     local handle = internet.request(repoPath .. p)
     for chunk in handle do result = result .. chunk end
     
+    handle.close()
     local f = io.open(p, "w")
     f:write(result)
     f:close()
   else
     fs.makeDirectory(p)
   end
+end
+
+do
+  local result = ""
+
+  local handle = internet.request(repoPath .. "/firmware.lua")
+  for chunk in handle do result = result .. chunk end
+  
+  handle.close()
+  EEPROMProxy.set(result)
+  EEPROMProxy.setLabel("B-BIOS")
 end
 
 computer.shutdown(true)
