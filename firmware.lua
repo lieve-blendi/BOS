16d95ab3369cec2f072c961021a8004c33785961
IonutParau <46377726+IonutParau@users.noreply.github.com>
2022-04-29 14:10:11 +0200

    Small firmware change

--- firmware.lua
+++ firmware.lua
@@ -18,31 +18,6 @@ do
         return boot_invoke(eeprom, "setData", address)
     end
 
-    local function boot(addr)
-        local width, height = boot_invoke(gpu, "getResolution")
-        boot_invoke(gpu, "fill", 1, 1, width, height)
-        boot_invoke(gpu, "set", 1, 1, "Booting " .. addr .. "...")
-        computer.setBootAddress(addr)
-        local handle, reason = boot_invoke(address, "open", "/init.lua")
-        if not handle then
-            return nil, reason
-        end
-        local buffer = ""
-        repeat
-            local data, reason = boot_invoke(address, "read", handle, math.huge)
-            if not data and reason then
-                return nil, reason
-            end
-            buffer = buffer .. (data or "")
-        until not data
-        boot_invoke(address, "close", handle)
-        local init, reason = load(buffer, "=init")
-        if not init then
-            error("Failed to load OS: " .. reason)
-        end
-        init()
-    end
-
     local screen = component.list("screen")()
     local gpu = component.list("gpu")()
     if gpu and screen then
@@ -54,6 +29,31 @@ do
     end
 
     while true do
+        local function tryLoadFrom(addr)
+            local width, height = boot_invoke(gpu, "getResolution")
+            boot_invoke(gpu, "fill", 1, 1, width, height)
+            boot_invoke(gpu, "set", 1, 1, "Booting " .. addr .. "...")
+            computer.setBootAddress(addr)
+            local handle, reason = boot_invoke(address, "open", "/init.lua")
+            if not handle then
+                return nil, reason
+            end
+            local buffer = ""
+            repeat
+                local data, reason = boot_invoke(address, "read", handle, math.huge)
+                if not data and reason then
+                    return nil, reason
+                end
+                buffer = buffer .. (data or "")
+            until not data
+            boot_invoke(address, "close", handle)
+            local init, reason = load(buffer, "=init")
+            if not init then
+                error("Failed to load OS: " .. reason)
+            end
+            init()
+        end
+
         local width, height = boot_invoke(gpu, "getResolution")
         boot_invoke(gpu, "fill", 1, 1, width, height)
         boot_invoke(gpu, "set", 1, 1, "Choose boot drive: ")
@@ -72,16 +72,15 @@ do
 
         if e == "key_down" then
             if code == 28 then
-                boot_invoke(gpu, "set", 1, 1, "You got mail")
-                boot(computer.getBootAddress())
+                tryLoadFrom(computer.getBootAddress())
             elseif code == 2 then
-                boot(fs[1])
+                tryLoadFrom(fs[1])
             elseif code == 3 then
-                boot(fs[2])
+                tryLoadFrom(fs[2])
             elseif code == 4 then
-                boot(fs[3])
+                tryLoadFrom(fs[3])
             elseif code == 5 then
-                boot(fs[4])
+                tryLoadFrom(fs[4])
             end
         end
     end
