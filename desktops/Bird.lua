d084bdeac97673316a8900b869ecad1e25eaa05d
IonutParau <46377726+IonutParau@users.noreply.github.com>
2022-04-29 12:27:45 +0200

    Fixed color issue

--- desktops/Bird.lua
+++ desktops/Bird.lua
@@ -9,7 +9,7 @@ function Bird:show()
   local sw,sy = Bird.RGPU.gpu.getResolution()
   Bird.RGPU:drawWithInstructions(
     {
-      {type = "rect", x = 1, y = 1, w = sw, h = 1, pixel = Bird.RGPU:pixel("0x1c76ba", " ", false)},
+      {type = "rect", x = 1, y = 1, w = sw, h = 1, pixel = Bird.RGPU:pixel(0x1c76ba, " ", false)},
       {type = "text", x = 1, y = 1, text = "ur mom"}
     }
   )
