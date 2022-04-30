a59b2d985c4f873ddaeba331d73ce456cdb8513d
IonutParau <46377726+IonutParau@users.noreply.github.com>
2022-04-30 20:24:41 +0200

    Fixed mouse input bug

--- firmware/network/NetworkInit.lua
+++ firmware/network/NetworkInit.lua
@@ -468,7 +468,7 @@ repeat
         local y = char
 
         if y > 0 and y <= #options then
-            if ((x > 0) and (x < #(options[y]))) then
+            if ((x > 0) and (x <= #(options[y]))) then
                 pointer = y
                 handleInput()
             end
