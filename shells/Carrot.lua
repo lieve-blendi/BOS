4a6a1836e8aa1c5ce1aaeee3cbf5da6e7f7d8df1
lieve-blendi <blendiappel@gmail.com>
2022-07-18 10:47:58 +0200

    added wget command to shell cuz why not

--- shells/Carrot.lua
+++ shells/Carrot.lua
@@ -5,6 +5,7 @@ Carrot.cmds = {}
 
 Carrot.name = "Carrot Shell"
 Carrot.version = "0.1"
+Carrot.internet = Components.internet
 
 function Carrot:bindCommand(command, callback)
     Carrot.cmds[command] = callback
@@ -27,5 +28,32 @@ end
 function Carrot:processString(str)
     
 end
+
+Carrot:bindCommand("wget",function(args)
+    local url = args[1]
+    local file = args[2]
+
+    if not url then
+        if type(DE.print) == "function" then
+            DE.print("Usage: wget <url> <file>")
+        end
+        return
+    end
+    if not file then
+        if type(DE.print) == "function" then
+            DE.print("Usage: wget <url> <file>")
+        end
+        return
+    end
+
+    if type(DE.print) == "function" then
+        DE.print("Downloading " .. url .. " to " .. file .. "...")
+    end
+    local result = ""
+    local handle = Carrot.internet.request(url)
+    for chunk in handle do result = result .. chunk end
+
+    FileSystem.write(file, result)
+end)
 
 return Carrot
